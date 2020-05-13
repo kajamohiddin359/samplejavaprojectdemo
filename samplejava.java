64f3c62929803e8a61fafe1fb452b4a400666a4f
kajamohiddin359 <kajaalimohiddin333@gmail.com>
2020-05-13 21:13:54 +0530

    commit second change

--- samplejava.java
+++ samplejava.java
@@ -1,6 +1,7 @@
 public class samplejava {
 	public static void main(String[]args){
 		System.out.println("Hi this is java program");
+		System.out.println("This is new change we are going to fire out for 13th may");
 	}
 
 }
