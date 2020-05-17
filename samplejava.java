dd1fc3a991ab21ceebb4b523992136dac797312f
kajamohiddin359 <kajaalimohiddin333@gmail.com>
2020-05-17 15:06:00 +0530

    changes push on 17th may

--- samplejava.java
+++ samplejava.java
@@ -2,6 +2,7 @@ public class samplejava {
 	public static void main(String[]args){
 		System.out.println("Hi this is java program");
 		System.out.println("This is new change we are going to fire out for 13th may");
+		System.out.println("This is new change we are going to introduce on 17th may")
 	}
 
 }
