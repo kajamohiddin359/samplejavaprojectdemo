679b327e05e97b6b492939ab6ab88b9ad3b1e2dc
kajamohiddin359 <kajaalimohiddin333@gmail.com>
2021-03-13 19:59:09 +0530

    adding new script to the repo

---
+++ sample.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+######################################################
+#   This is sample shell script for execution jenkins#
+######################################################
+
+echo "Hello World..."
+echo "Successfully executed the data...."
